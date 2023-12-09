56496910e01fb9b9633a0a03c509df8001fc52b1
demonking15543 <demonking15543@gmail.com>
2023-12-09 14:22:48 +0530

    logo and button added

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,22 @@ class StartScreen extends StatelessWidget{
 
   @override  
   Widget build(context){
-    return const Center(child:  Text("hello"));
+    return  Center(child:  Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+
+         Opacity(opacity: 0.7, child:Image.asset("assets/images/logo.png", width: 200,),
+),
+     const SizedBox(height:18),
+     const  Text("Learn flutter in fun way", style: TextStyle(color: Colors.white, fontSize: 20),),
+          const SizedBox(height:30),
+          OutlinedButton.icon(
+            onPressed: (){},
+            icon: const Icon(Icons.arrow_right_alt),
+            
+            style:OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label:const  Text("Start Quiz"),)
+
+    ],));
   }
 }
