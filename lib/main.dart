50602f8d32936c9ad8fe209f6eaace3983c15bc6
demonking15543 <demonking15543@gmail.com>
2023-12-09 13:30:23 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(const QizeApp());
+}
+
+class QizeApp extends StatelessWidget {
+  const QizeApp({super.key});
+  @override
+  Widget build(context) {
+    return  MaterialApp(
+      home: Scaffold(
+        body:  Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors:[
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168)
+
+
+          ])),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
