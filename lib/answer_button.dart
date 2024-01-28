c6fd280ebf7c3c8f681fd19ebf46caba66352180
demonking15543 <demonking15543@gmail.com>
2024-01-28 20:15:43 +0530

    add more questions

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -11,14 +11,19 @@ class AnswerButton extends StatelessWidget{
  final void Function() onTap;
 @override
 Widget build(BuildContext context){
-  return ElevatedButton(onPressed: onTap,
-         style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40),)
-         ),
-          child:  Text(answerText, textAlign: TextAlign.center,));
+  return Container(
+    margin: EdgeInsets.all(4),
+    child: ElevatedButton(onPressed: onTap,
+           style: ElevatedButton.styleFrom(
+          
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+            
+            backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40),)
+           ),
+            child:  Text(answerText, textAlign: TextAlign.center,)),
+  );
          
 }
 }
